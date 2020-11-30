47f4d9b7e836babfa12d53b645f15d9038589ebe
Leo Kim <gh.leokim@gmail.com>
2020-11-30 12:56:51 +0900

    Create .py file automatically

--- algoNewday.sh
+++ algoNewday.sh
@@ -3,9 +3,18 @@
 today=$(date +"%y%m%d")
 cur=$(pwd)
 
+# check if directory exists
 if [ ! -d ${cur}/${today} ]; then
 	mkdir -p ${cur}/${today}
 	echo "directory ${today}/ made"
 else
 	echo "directory ${today} already exists."
+fi
+
+# check if test file exists
+if [ ! -f ${cur}/${today}/test.py ]; then
+	touch ${cur}/${today}/test.py
+	echo "file ${today}/test.py made"
+else
+	echo "file ${today}/test.py already exists."
 fi
